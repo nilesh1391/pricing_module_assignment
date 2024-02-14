f616ffc2164cf8c0b9c6f5d667f145af55ed34fe
Nilesh Kumar Tiwari <nileshtiwari1391@gmail.com>
2024-02-14 18:26:26 +0530

    resolving type error in price calculation

--- pricing/views.py
+++ pricing/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import PricingConfig
+from decimal import Decimal
 
 # Create your views here.
 def calculate_price(request):
     if request.method== 'GET':
-        distance_traveled = float(request.GET.get('distance_traveled', 0))
-        time_traveled = float(request.GET.get('time_traveled', 0))
-        waiting_time = float(request.GET.get('waiting_time', 0))
+        distance_traveled = Decimal(request.GET.get('distance_traveled', 0))
+        time_traveled = Decimal(request.GET.get('time_traveled', 0))
+        waiting_time = Decimal(request.GET.get('waiting_time', 0))
 
         day_of_week = request.GET.get('day_of_week', 'Mon')
 
@@ -16,12 +17,19 @@ def calculate_price(request):
         except PricingConfig.DoesNotExist:
             return JsonResponse({"error": "No pricing configuration found for the provided day of the week"}, status=400)
         
-        dbp = pricing_config.distance_base_price
-        dap = pricing_config.distance_additional_price
-        tmf = pricing_config.time_multiplier_factor
-        wc = pricing_config.waiting_charges
+        dbp = Decimal(pricing_config.distance_base_price)
+        dap = Decimal(pricing_config.distance_additional_price)
+        tmf = Decimal(pricing_config.time_multiplier_factor)
+        wc = Decimal(pricing_config.waiting_charges)
 
-        price = (dbp + (max(distance_traveled - 3, 0)*dap)) + (max(time_traveled - 60, 0)* tmf)+(waiting_time//3*wc)
+        # Perform calculations using Decimal objects
+        additional_distance_charge = max(distance_traveled - Decimal('3'), Decimal('0')) * dap
+        time_multiplier_charge = max(time_traveled - Decimal('60'), Decimal('0')) * tmf
+        waiting_charge = (waiting_time // Decimal('3')) * wc
+
+        # Calculate total price
+        price = (dbp + additional_distance_charge) + time_multiplier_charge + waiting_charge
+
         return JsonResponse({"price": price})
     else:
         return JsonResponse({"error": "Method not allowed"}, status=405)
